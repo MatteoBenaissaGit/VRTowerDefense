526a1c307699889bed092b9f97ee5916bc25b382
SimonWalenczak <92367148+SimonWalenczak@users.noreply.github.com>
2023-09-26 15:59:16 +0200

    Update WaveData

--- Assets/Data/Wave/WaveData.cs
+++ Assets/Data/Wave/WaveData.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Enemy;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Wave", order = 2)]
 public class WaveData : ScriptableObject
 {
     [Header("Timer Waves")]
-    public float TimeBetweenWaves;
-    public float ActualTimeBetweenWaves;
+    public float ActualTimeBetweenWave;
     public bool CanSpawnTroop;
 
     public List<Wave> Waves;
